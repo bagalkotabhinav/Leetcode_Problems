d6f8d974137d9d195dc5b099113f611ee6988fa5
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2023-08-12 16:26:45 +0530

    Create 152.max_product_subarray.cpp

---
+++ Medium/152.max_product_subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int p=1,maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            p*=nums[i];
+            maxi=max(p,maxi);
+            if(p==0)
+                p=1;
+        }
+        p=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            p*=nums[i];
+            maxi=max(p,maxi);
+                 if(p==0)
+               p=1;
+      }
+        return maxi;
+    }
+};
