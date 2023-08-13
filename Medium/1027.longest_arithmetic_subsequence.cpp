fbef0a393b7a773d75009ee453eaa3c2a3d3f75b
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2023-08-14 00:40:22 +0530

    Create 1027.longest_arithmetic_subsequence.cpp

---
+++ Medium/1027.longest_arithmetic_subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=2){
+            return n;
+        }
+        vector<unordered_map<int,int>>dp(n);
+        int longest;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int diff=nums[i]-nums[j];
+                if(dp[j].count(diff)){
+                    dp[i][diff]=dp[j][diff]+1;
+                }
+                else{
+                    dp[i][diff]=2;
+                }
+                longest=max(longest,dp[i][diff]);
+
+            }
+        }
+        return longest;
+    }
+};
